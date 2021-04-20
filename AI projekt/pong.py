3af97505c8da2468ebf43c5a0f1e16b2746931ff
AbbMyBjo <54663051+AbbMyBjo@users.noreply.github.com>
2021-04-20 14:13:29 +0200

    buggfix

--- AI projekt/pong.py
+++ AI projekt/pong.py
@@ -4,6 +4,7 @@ import sys
 import math
 import random 
 
+# Formatera bakgrund m.m.
 backgroundColor = (201,255,229)
 width, height = 500, 600
 screen = pygame.display.set_mode((width, height))
@@ -11,44 +12,31 @@ pygame.display.set_caption('Pong demo')
 screen.fill(backgroundColor)
 clock = pygame.time.Clock()
 
+AIpoints = 0
+playerPoints = 0
+
 q = pygame.QUIT
 main = True
 Color = (0, 0, 0)
-radie = 5
+radius = 5
 game = True
 
-pygame.init()
+pygame.init() # Starta pygame
 
 # jokerman/cooperblack(badboll), gigi(fantasy), Chopsic.ttf(space), nes(standard)
 font = pygame.font.SysFont("nes", 60)
 btnFont = pygame.font.SysFont("nes", 30)
 # font = pygame.font.Font('Chopsic.ttf', 60)
 
-text = font.render("Game over!", True, (0, 0, 0))
+winText = font.render("Congratulations!", True, (0, 0, 0))
+lossText = font.render("Game over!", True, (0, 0, 0))
+drawText = font.render("It's a draw!", True, (0, 0, 0))
 btnText = btnFont.render("Try again", True, (255, 255, 255))
 
 class Ball:
     def __init__(self, color, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.draw.circle(screen, (0, 0, 0), (circlePosX, circlePosY), 2*radie)
-
-
-# class invisibleBall:
-#     def __init__(self, vector):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image, self.rect = load_png('ball.png')
-#         # screen = pygame.display.get_surface()
-#         self.area = screen.get_rect()
-#         self.vector = vector
-
-#     def update(self):
-#         newpos = self.calcnewpos(self.rect, self.vector)
-#         self.rect = newpos
-
-#     def calcnewpos(self, rect, vector):
-#         (angle, z) = vector
-#         (dx, dy) = (z*math.cos(angle), z*math.sin(angle))
-#         return rect.move(dx, dy)
+        self.image = pygame.draw.circle(screen, (0, 0, 0), (circlePosX, circlePosY), 2*radius)
 
 while main:
     done = False
@@ -63,10 +51,17 @@ while main:
         screen.fill(backgroundColor)
         pygame.draw.line(screen, (0, 0, 0), (0, 505), (500, 505), 5)
         player1 = pygame.draw.rect(screen, Color, (30, paddlePosY, 10, 50))
-        player2 = pygame.draw.rect(screen, Color, (470, AIpaddlePosY, 10, 50)) # (x kordinat, y kordinat, bredd, längd)
+        AI = pygame.draw.rect(screen, Color, (470, AIpaddlePosY, 10, 50)) # (x kordinat, y kordinat, bredd, längd)
+        pygame.draw.line(screen, (0, 0, 0), (249, 0), (249, 600), 2)
+        printPoint = font.render(str(playerPoints), True, (0, 0, 0))
+        printPointAI = font.render(str(AIpoints), True, (0, 0, 0))
+
+        screen.blit(printPoint, (200 - printPoint.get_width() //2, 550 - printPoint.get_height() // 2))
+        screen.blit(printPointAI, (300 - printPointAI.get_width() //2, 550 - printPointAI.get_height() // 2))
+
         ball = Ball(Color, 5, 5)
 
-        clock.tick(500)
+        clock.tick(500) # Timer för att sakta ner spelet
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -117,68 +112,74 @@ while main:
         else:
             direction = 0
 
-        if circlePosX >= 500-2*radie:
-            if circlePosY >= 500-2*radie:
-                diffY = -diffY
-            elif circlePosY <= 0+2*radie:
-                diffY = -diffY
-            diffX = -diffX
-
-        elif circlePosX <= 0+2*radie:
-            if circlePosY >= 500-2*radie:
-                diffY = -diffY
-            elif circlePosY <= 0+2*radie:
-                diffY = -diffY
-            diffX = -diffX
-        
-        elif circlePosY >= 500-2*radie:
-            if circlePosX >= 500-2*radie:
-                diffX = -diffX
-            elif circlePosX <= 0+2*radie:
-                diffX = -diffX
-            diffY = -diffY
-        
-        elif circlePosY <= 0+2*radie: 
-            if circlePosX >= 500-2*radie:
-                diffX = -diffX
-            elif circlePosX <= 0+2*radie:
-                diffX = -diffX
+        # Om den krockar med vägg i Y-led
+        if circlePosY >= 500 or circlePosY <= 0:
             diffY = -diffY
 
+        # Om den krockar med vänster padel
         if circlePosX == 50:
-            if circlePosY >= paddlePosY and circlePosY <= paddlePosY+50:
-                diffX = -diffX
-            # elif circlePosY <= AIpaddlePosY and circlePosY >= AIpaddlePosY-50:
-            #     diffX = -diffX
+            if circlePosY >= paddlePosY-5 and circlePosY <= paddlePosY+55:
+                diffX = -diffX  # olika vinklar här
 
+        # Höger padel
+        elif circlePosX == 450:
+            if circlePosY >= AIpaddlePosY-5 and circlePosY <= AIpaddlePosY+55:
+                diffX = -diffX # och här
+
+        # Krock med vägg i X-led
         elif circlePosX == 10 or circlePosX == 490:
-            game = False
+            if playerPoints < 5 and AIpoints < 5: 
+                if circlePosX == 10:
+                    AIpoints += 1
+                elif circlePosX == 490:
+                    playerPoints += 1
+                game = False
             #skriv ut game over över skärmen och avsluta spelet
-
+        
+        # Addera skillnader till bollens koordinater för förflyttning
         circlePosX += diffX
         circlePosY += diffY
         paddlePosY += direction
         pygame.display.flip()
-    
-    screen.blit(text,(250 - text.get_width() // 2, 250 - text.get_height() // 2))
-    button = pygame.draw.rect(screen, (0, 0, 0), (180, 280, 140, 30))
-    screen.blit(btnText, (250 - btnText.get_width() //2, 295 - btnText.get_height() // 2))
-    pygame.display.flip()
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == ord('q'):
-                    pygame.quit()
-                    sys.exit()
-                    done = True
-                    main = False
 
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                # 1 is the left mouse button, 2 is middle, 3 is right.
-                if event.button == 1:
-                    # `event.pos` is the mouse position.
-                    if button.collidepoint(event.pos):
+    # Om någon vunnit skriv ut det till player1
+    if AIpoints == 5 or playerPoints == 5:
+        if AIpoints == 5:
+            screen.blit(lossText,(250 - lossText.get_width() // 2, 250 - lossText.get_height() // 2))
+        
+        elif playerPoints == 5:
+            screen.blit(winText, (250 - winText.get_width() //2, 250 - winText.get_height() // 2))
+        
+        else:
+            screen.blit(drawText, (250 - drawText.get_width() //2, 250 - drawText.get_height() // 2))
+
+        button = pygame.draw.rect(screen, (0, 0, 0), (180, 280, 140, 30))
+        screen.blit(btnText, (250 - btnText.get_width() //2, 295 - btnText.get_height() // 2))
+        pygame.display.flip()
+
+        # Try again knapp @click
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == ord('q'):
+                        pygame.quit()
+                        sys.exit()
                         done = True
-                        game = True
+                        main = False
 
-# en osynlig boll som går snabbare än den synliga så AI:n vet var den riktiga ska och kan åka dit snabbare
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    # 1 är vänsterklick, 2 mitten, 3 höger
+                    if event.button == 1:
+                        # `event.pos` är muspekarens position
+                        if button.collidepoint(event.pos):
+                            # Gå ut ur "done" loopen och in i "game" loopen
+                            done = True
+                            game = True
+        
+        # Återställning av poäng om nån vunnit
+        AIpoints = 0
+        playerPoints = 0    
+
+    # Om ingen vunnit hela spelet än men vunnit en omgång
+    else:
+        game = True
